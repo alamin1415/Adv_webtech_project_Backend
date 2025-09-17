c08babaa051a33b853cf87d7d86a72e98f25cb5f
WHO_AM_I <106102681+alamin1415@users.noreply.github.com>
2025-09-17 21:39:57 +0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // সব API এবং সব ডোমেইনের জন্য CORS সক্রিয় করা
+  
   app.enableCors({
-    origin: '*', // সব ডোমেইন থেকে অনুমোদন
+    origin: '*', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true, // যদি cookies ব্যবহার করা হয়
+    credentials: true, 
   });
 
-  // PORT environment variable বা default 3000 ব্যবহার করা
+
   const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
   await app.listen(port);
 
